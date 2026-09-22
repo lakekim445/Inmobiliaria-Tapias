0ca1e55ff773f2d4117a7a154818602cd356695f
Isabel <kimberlyisabellagostapia@gmail.com>
2026-09-22 12:24:04 -0400

    se agrego register dto

---
+++ InmobiliariaAPI/DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace InmobiliariaAPI.DTOs
+{
+    public class RegisterDTO
+    {
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
